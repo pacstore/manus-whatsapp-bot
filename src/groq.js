581b6f7f453b6c987f2fbd0aed68938e5a91d157
pacstore <107905955+pacstore@users.noreply.github.com>
2026-01-16 12:23:13 -0300

    Update groq.js

--- src/groq.js
+++ src/groq.js
@@ -1,4 +1,4 @@
-import Groq from 'groq-sdk';
+const Groq = require('groq-sdk');
 
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY
@@ -51,4 +51,4 @@ Importante:
   }
 }
 
-export { askGroq };
+module.exports = { askGroq };
